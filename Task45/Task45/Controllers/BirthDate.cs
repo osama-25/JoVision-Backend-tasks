c45b4b030b58150d5c2790cdc3ebecd9009cfe34
osama-25 <osamasami208@gmail.com>
2024-08-13 12:34:42 +0300

    Task-45

---
+++ Task45/Task45/Controllers/BirthDate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task45.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirthDate : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] int year, [FromQuery] int month, [FromQuery] int day, [FromQuery] string name = "")
+        {
+            if (name == "") name = "anonymous";
+
+            if (year == 0 || month == 0 || day == 0)
+                return Ok($"Hello {name},  I can’t calculate your age without knowing your birthdate!");
+            else
+            {
+                int age = DateTime.Today.Year - year;
+                int nowmonth = DateTime.Today.Month;
+                int nowday = DateTime.Today.Day;
+                if(month > nowmonth || (month == nowmonth && day > nowday))
+                {
+                    age--;
+                }
+
+                return Ok($"Hello {name}, your age is {age}");
+            }
+
+        }
+    }
+}
---
+++ Task45/Task45/Controllers/Greeter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task44.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Greeter : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string name = "")
+        {
+            if (name == "") name = "anonymous";
+            return Ok($"Hello {name}");
+        }
+    }
+}
